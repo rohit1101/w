73b14f892e47334cc362dd0e32642e1532b45b6a
Rohit <srohit1101@gmail.com>
2020-04-29 22:14:48 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import lodash from 'lodash';
+import './style.css';
+import Picture from '../images/image.png';
+
+const addComponent = component => document.body.appendChild(component);
+
+const textComponent = text => {
+  const element = document.createElement('div');
+  element.innerHTML = lodash.join(text, ' ');
+  element.classList.add('hello');
+  addComponent(element);
+};
+
+const imageComponent = image => {
+  const img = new Image();
+  img.src = image;
+  addComponent(img);
+};
+
+textComponent(['Hello', 'World']);
+imageComponent(Picture);
+
+// console.log(styles);
+// console.log(duplicate);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
+};
